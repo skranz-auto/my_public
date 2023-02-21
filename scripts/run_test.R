fe5e15955499ac520f32fb1544c6d9cc1493813d
skranz-auto <xsx50@uni-ulm.de>
2023-02-21 11:07:50 +0100

    update

--- scripts/run_test.R
+++ scripts/run_test.R
@@ -4,19 +4,6 @@ library(reticulate)
 reticulate::source_python("~/scripts/hello.py")
 hello_py()
 
-cat("\nDownload OI package\n")
-
-reticulate::source_python("~/scripts/download_oicpsr.py")
-
-login_email = Sys.getenv("OI_EMAIL")
-login_password = Sys.getenv("OI_PASSWORD")
-outfile = "oi.zip"
-outdir = '/root/output'
-pid = "112829"
-
-download_oicpsr(pid,login_email,login_password,outfile,outdir)
-
-
 cat("\nCheck Secrets\n")
 
 cat('\n\nSys.getenv("MYSECRET") = ', Sys.getenv("MYSECRET"))
